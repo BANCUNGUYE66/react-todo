1a9494eacbb06d34a5e529dd0ab28d62b5d56d96
Aimable BANCUNGUYE <bancunguye66@gmail.com>
2023-05-04 14:01:06 +0300

    add delete feature

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '@/components/Header';
+import TodosLogic from '@/components/TodosLogic';
+
+    
+    const TodoApp = () => {
+      return (
+        <>
+           <Header />
+           <TodosLogic />
+        </>
+      );
+    };
+    export default TodoApp;
---
+++ src/components/TodosLogic.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import InputTodo from '@/components/InputTodo';
+import TodosList from '@/components/TodosList';
+
+const TodosLogic = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Setup development environment',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Develop website and add content',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Deploy to live server',
+      completed: false,
+    },
+  ]);
+  
+  const handleChange = (id) => {
+    setTodos((prevState) =>
+      prevState.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      })
+    );
+  };
+
+  const delTodo = (id) => {
+    setTodos([
+      ...todos.filter((todo) => {
+        return todo.id !== id;
+      }),
+    ]);
+  };
+
+  return (
+    <div>
+      <InputTodo />
+      <TodosList
+        todosProps={todos}
+        handleChange={handleChange}
+        delTodo={delTodo}
+      />
+    </div>
+  );
+};
+
+export default TodosLogic;
